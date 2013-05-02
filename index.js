df5044f9130aeeb3e0d107c86af3ca89d1a9322e
jgranstrom <granstrom.john@gmail.com>
2013-05-02 17:07:55 +0200

    Some convention fixes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* Usage for the file test.mp4 */
-new (require('./lib/streamsha').Streamsha)().hashFile('test.bin', function(hash, buffer) {
+new (require('./lib/streamsha').Streamsha)().hashFile({ path: 'test.bin', encoding: 'hex' }, 
+	function(err, hash, buffer) {
+		if(err) throw err;
     console.log(hash);
     console.log(buffer);
-}, 'hex');
+});
